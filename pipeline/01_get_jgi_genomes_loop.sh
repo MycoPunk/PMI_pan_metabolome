b19949c7830a9d8a46e332648c07749129634a83
Lotus Lofgren <lal76@dcc-login-03.oit.duke.edu>
2022-04-15 11:48:35 -0400

    updated 01 script to put files in correct folders

--- pipeline/01_get_jgi_genomes_loop.sh
+++ pipeline/01_get_jgi_genomes_loop.sh
@@ -16,15 +16,19 @@
 #get fasta
 #python3 scripts/jgi-query.py Umbelo1 -r _AssemblyScaffolds.fasta.gz 
 
+cd genomes/
+
 while read JGI_CODE; do
 
-cd  genomes/$JGI_CODE/gff
+cd  $JGI_CODE/gff
 #put them in the right place
 #get gff3
-python3 ~/PMI_metaSMC/scripts/jgi-query.py $JGI_CODE -r .gff3.gz
+python3 /hpc/group/vilgalyslab/lal76/PMI_metaSMC/scripts/jgi-query.py $JGI_CODE -r .gff3.gz
 
-cd  ../../genomes/$JGI_CODE/fasta
+cd  ../fasta/
 #get fasta
-python3 ~/PMI_metaSMC/scripts/jgi-query.py $JGI_CODE -r _AssemblyScaffolds.fasta.gz; 
+python3 /hpc/group/vilgalyslab/lal76/PMI_metaSMC/scripts/jgi-query.py $JGI_CODE -r _AssemblyScaffolds.fasta.gz; 
+cd ../../
 
-done < genomes.txt
+done < ../genomes.txt
+
